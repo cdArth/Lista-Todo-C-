4f41ab196553c2bd1e768fd5ae559af9c3556355
cdArth <88564736+cdArth@users.noreply.github.com>
2022-02-24 14:42:08 -0300

    Add files via upload

---
+++ Projeto de listas - 17_02/Item.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_de_listas___17_02
+{
+    public class Item
+    {
+        //        Atributos: 
+
+        //Id to tipo inteiro
+        //Titulo do tipo string
+        //Descricao do tipo String
+        //Feito do tipo boolean
+
+        public int id { get; set; }
+        public string titulo { get; set; }
+        public string descricao { get; set; }
+        public Boolean feito { get; set; }
+
+        //Construtor
+        //Que recebe uma string e já atribui ao Titulo e seta Feito para false. 
+
+        public Item(string t)
+        {
+            titulo = t;
+            feito = false;
+        }
+
+
+
+
+    }
+
+}
---
+++ Projeto de listas - 17_02/Library.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_de_listas___17_02
+{
+    public class Library
+    {
+        static string connStr =
+            ConfigurationManager.ConnectionStrings["DTM"].ConnectionString;
+        public List<Item> ObterItens(int id)
+        {
+            List<Item> items = new List<Item>();
+            Item result;
+            var strConn = connStr;
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+
+                conn.Open();
+                using (SqlCommand command = conn.CreateCommand())
+                {
+                    command.CommandText = $"select * from Itens" +
+                        $" where TodoId = @pId";
+                    command.Parameters.AddWithValue("pId", id);
+
+                    using (var reader = command.ExecuteReader())
+                        while (reader.Read())
+                        {
+                            result = new Item("");
+                            result.id = Convert.ToInt32(reader["Id"]);
+                            result.titulo = reader["Titulo"].ToString();
+                            result.feito = Convert.ToBoolean(reader["Feito"]);
+                            items.Add(result);
+                        }
+                }
+
+            };
+
+            return items;
+        }
+        public List<Todo> ObterListas()
+        {
+            List<Todo> result = new List<Todo>();
+            Todo todo;
+            var strConn = connStr;
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+
+                conn.Open();
+                using (SqlCommand command = conn.CreateCommand())
+                {
+                    command.CommandText = $"select * from Todos";
+
+                    using (var reader = command.ExecuteReader())
+                        while (reader.Read())
+                        {
+                            todo = new Todo();
+                            todo.id = Convert.ToInt32(reader["Id"]);
+                            todo.Nome = reader["Nome"].ToString();
+                            result.Add(todo);
+
+
+                        }
+                }
+
+            };
+
+            return result;
+        }
+        public void AdicionarLista(string nome)
+        {
+            var strConn = connStr;
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+
+                conn.Open();
+                using (SqlCommand command = conn.CreateCommand())
+                {
+                    command.CommandText = $"insert into Todos (Nome) values (@pNome)";
+                    command.Parameters.AddWithValue("pNome", nome);
+                    command.ExecuteNonQuery();
+                }
+
+            };
+
+        }
+        public void AdicionarItem(Item item)
+        {
+            var strConn = connStr;
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+
+                conn.Open();
+                using (SqlCommand command = conn.CreateCommand())
+                {
+                    command.CommandText = $"insert into Itens (Titulo,TodoId) values (@pTitulo,@pTodoId)";
+                    command.Parameters.AddWithValue("pTitulo", item.titulo);
+                    command.Parameters.AddWithValue("pTodoId", item.id);
+                    command.ExecuteNonQuery();
+                }
+
+            };
+
+        }
+        public void ExcluirItem(int id)
+        {
+            var strConn = connStr;
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+
+                conn.Open();
+                using (SqlCommand command = conn.CreateCommand())
+                {
+                    command.CommandText = $"delete from Itens where Id=@pId";
+                    command.Parameters.AddWithValue("pId", id);
+                    command.ExecuteNonQuery();
+                }
+
+            };
+
+        }
+
+        public void EditarItem(Item item)
+        {
+            var strConn = connStr;
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+
+                conn.Open();
+                using (SqlCommand command = conn.CreateCommand())
+                {
+                    command.CommandText = $"update Itens set Titulo=@pTitulo where Id=@pId";
+                    command.Parameters.AddWithValue("pId", item.id);
+                    command.Parameters.AddWithValue("pTitulo", item.titulo);
+                    command.ExecuteNonQuery();
+                }
+
+            };
+
+        }
+        public void MarcarConcluido(int id)
+        {
+            var strConn = connStr;
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+
+                conn.Open();
+                using (SqlCommand command = conn.CreateCommand())
+                {
+                    command.CommandText = $"update Itens set Feito = 1 where Id=@pId";
+                    command.Parameters.AddWithValue("pId", id);
+                    command.ExecuteNonQuery();
+                }
+
+            };
+
+        }
+        public void DesmarcarConcluido(int id)
+        {
+            var strConn = connStr;
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+
+                conn.Open();
+                using (SqlCommand command = conn.CreateCommand())
+                {
+                    command.CommandText = $"update Itens set Feito = 0 where Id=@pId";
+                    command.Parameters.AddWithValue("pId", id);
+                    command.ExecuteNonQuery();
+                }
+
+            };
+        }
+    }
+}
